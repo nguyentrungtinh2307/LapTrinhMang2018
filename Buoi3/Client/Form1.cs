96f18089b8583af3eebdfeb1ac0077e0b6dd58e6
Nguyen Tinh <nguyentrungtinh2307@gmail.com>
2018-03-23 20:52:30 +0700

    Bai TH buoi 3

---
+++ Buoi3/Client/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+        EndPoint remote;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("192.168.1.28"), 995);
+            remote = (EndPoint)ipe;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Kéo";
+            byte[] sendData = Encoding.ASCII.GetBytes("0");
+
+            client.SendTo(sendData, remote);
+            byte[] receiveData = new byte[20];
+            client.ReceiveFrom(receiveData, ref remote);
+            int recv = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+
+            if (recv == 0)
+                textBox2.Text = "Hòa";
+            else if (recv == 1)
+                textBox2.Text = "Thắng";
+            else
+                textBox2.Text = "Thua";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Búa";
+            byte[] sendData = Encoding.ASCII.GetBytes("2");
+
+            client.SendTo(sendData, remote);
+            byte[] receiveData = new byte[20];
+            client.ReceiveFrom(receiveData, ref remote);
+            int recv = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+ 
+            if (recv == 0)
+                textBox2.Text = "Thắng";
+            else if (recv == 1)
+                textBox2.Text = "Thua";
+            else
+                textBox2.Text = "Hòa";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bao";
+            byte[] sendData = Encoding.ASCII.GetBytes("1");
+
+            client.SendTo(sendData, remote);
+            byte[] receiveData = new byte[20];
+            client.ReceiveFrom(receiveData, ref remote);
+            int recv = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+
+            if (recv == 0)
+                textBox2.Text = "Thua";
+            else if (recv == 1)
+                textBox2.Text = "Hòa";
+            else
+                textBox2.Text = "Thắng";
+        }
+    }
+}
---
+++ Buoi3/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 995);
+            server.Bind(ipe);
+            EndPoint remote = ipe;
+            Random rd = new Random();
+
+            while (true)
+            {
+                byte[] receiveData = new byte[1024];
+                server.ReceiveFrom(receiveData, ref remote);
+                int clientResult = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+                string serverResult = rd.Next(0, 3).ToString();
+                byte[] sendData = Encoding.ASCII.GetBytes(serverResult);
+                if (serverResult == "2")
+                    Console.WriteLine("_ Búa");
+                else if (serverResult == "1")
+                    Console.WriteLine("_ Bao");
+                else
+                    Console.WriteLine("_ Kéo");
+                server.SendTo(sendData, remote);
+            }
+        }
+    }
+}
