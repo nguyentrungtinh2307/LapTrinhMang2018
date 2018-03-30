d4b57407d496d6a04762ff537c453ab26e2566b0
Nguyen Tinh <nguyentrungtinh2307@gmail.com>
2018-03-30 22:29:24 +0700

    Bai TH buoi 4

---
+++ Buoi4/Bai2-UDP/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace Bai2_UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string IP = "192.168.1.25";
+            int port = 80;
+
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+
+            Thread receiveThread = new Thread(ReceiveData);
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+
+            UdpClient client = new UdpClient();
+
+            try
+            {
+                string text;
+                do
+                {
+                    text = Console.ReadLine();
+
+                    if (text.Length != 0)
+                    {
+                        byte[] data = Encoding.UTF8.GetBytes(text);
+                        client.Send(data, data.Length, remoteEndPoint);
+                    }
+                } while (text.Length != 0);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private static void ReceiveData()
+        {
+            UdpClient client = new UdpClient(80);
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] data = client.Receive(ref anyIP);
+                    string text = Encoding.UTF8.GetString(data);
+                    Console.WriteLine("--> " + text);
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Buoi4/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress broadcast = IPAddress.Parse("192.168.52.1");
+            byte[] sendbuf = Encoding.ASCII.GetBytes("TESTTTT");
+            IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+            s.SendTo(sendbuf, ep);
+        }
+    }
+}
